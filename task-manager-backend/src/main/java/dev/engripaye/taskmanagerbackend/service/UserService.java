35a2545a7e684cc5323574ddaef636c5df3311cd
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-14 11:19:48 +0100

    backend code commit by ENGR. IPAYE

--- task-manager-backend/src/main/java/dev/engripaye/taskmanagerbackend/service/UserService.java
+++ task-manager-backend/src/main/java/dev/engripaye/taskmanagerbackend/service/UserService.java
@@ -1,9 +1,28 @@
 package dev.engripaye.taskmanagerbackend.service;
 
+import dev.engripaye.taskmanagerbackend.repository.UserRepository;
+import dev.engripaye.taskmanagerbackend.security.JwtService;
+import dev.engripaye.taskmanagerbackend.security.UserDetailsServiceImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager, UserDetailsServiceImpl userDetailsService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+    }
 
 
 }
