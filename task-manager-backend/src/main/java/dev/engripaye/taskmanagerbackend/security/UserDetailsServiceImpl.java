db2f7c68dd7a78f360b1f57f1a375c65b6c3d05e
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-14 11:02:47 +0100

    backend code commit by ENGR. IPAYE

---
+++ task-manager-backend/src/main/java/dev/engripaye/taskmanagerbackend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package dev.engripaye.taskmanagerbackend.security;
+
+
+import dev.engripaye.taskmanagerbackend.entity.User;
+import dev.engripaye.taskmanagerbackend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
