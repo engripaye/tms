704c14f02527c9890c3c06bf16c2096980891414
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-18 09:04:49 +0100

    backend code commit by ENGR. IPAYE

--- task-manager-backend/src/main/java/dev/engripaye/taskmanagerbackend/controller/AuthController.java
+++ task-manager-backend/src/main/java/dev/engripaye/taskmanagerbackend/controller/AuthController.java
@@ -1,12 +1,40 @@
 package dev.engripaye.taskmanagerbackend.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.engripaye.taskmanagerbackend.dtos.AuthResponse;
+import dev.engripaye.taskmanagerbackend.dtos.LoginRequest;
+import dev.engripaye.taskmanagerbackend.dtos.RegisterRequest;
+import dev.engripaye.taskmanagerbackend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin(origins = "*")  //for frontend purpose
 public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // REGISTER
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request){
+        userService.register(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body("User registered successfully");
+    }
+
+    // LOGIN
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+        AuthResponse authResponse = userService.login(request);
+        return ResponseEntity.ok(authResponse);
+    }
+
 }
