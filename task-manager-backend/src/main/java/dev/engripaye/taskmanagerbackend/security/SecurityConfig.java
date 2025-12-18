ccc234a705135e6d9374a448251bb3faea26bcf7
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-18 08:59:39 +0100

    backend code commit by ENGR. IPAYE

--- task-manager-backend/src/main/java/dev/engripaye/taskmanagerbackend/security/SecurityConfig.java
+++ task-manager-backend/src/main/java/dev/engripaye/taskmanagerbackend/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package dev.engripaye.taskmanagerbackend.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 
+@Configuration
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
